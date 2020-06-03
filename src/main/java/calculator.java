1e37b83b0d0c014dbc77d0c9f77f6c474b562059
dziarskihank <dziarskihank@users.noreply.github.com>
2020-06-03 16:50:45 +0200

    first commit

---
+++ src/main/java/calculator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class calculator
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        int a = 30;
+
+        int b = 6;
+
+        int sum = a + b;
+
+        System.out.println("Dodawanie:" + sum );
+
+        int c = 2;
+        int d = 4;
+        int sum2 = c-d;
+
+        System.out.println("Odejmowanie:"+ sum2);
+    }
+}
